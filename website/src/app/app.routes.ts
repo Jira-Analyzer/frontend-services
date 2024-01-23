e0683728ea6c3d33270df34b8c057ae1012c2420
Ridof <georgetsalikov17@gmail.com>
2024-01-23 12:59:19 +0300

    #2 web

---
+++ website/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  //{
+  //path: 'home',
+  //component:
+  //},
+  //{
+  //path: 'about-us',
+  //component:
+  //}
+];
