f019c48baf3183b4caa54eb985a92743c1024682
Ridof <georgetsalikov17@gmail.com>
2024-01-25 08:51:45 +0300

    WEB status #3

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ComparisonComponent } from './pages/comparison/comparison.component';
+import { MyprojComponent } from './pages/myproj/myproj.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, ProjectsComponent, ComparisonComponent, MyprojComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ website/src/app/pages/myproj/myproj.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myproj',
+  standalone: true,
+  imports: [],
+  templateUrl: './myproj.component.html',
+  styleUrl: './myproj.component.css'
+})
+export class MyprojComponent {
+
+}
